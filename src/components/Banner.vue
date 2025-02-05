01caebb620c848a677b20552f15d9b314a027a41
Gustavo Nunes Wawrick <gustavo_nw@outlook.com>
2025-02-05 20:08:40 -0300

    Criado um novo arquivo de ConteudoPrincipal e realizado alguns ajustes nos arquivos de vue já existentes no código

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,16 +1,13 @@
 <template>
-    <header class="banner">
+  <header class="banner">
     <div class="apresentacao">
       <img src="../assets/imagens/logo.svg" alt="Logo do Cookin' Up" class="logo">
 
       <p class="cabecalho-lg frase-cabecalho">
-        <span class="texto-verde">Um banquete de ideias para</span>
-        despertar o chef que há em você!
+        <span class="texto-verde">Um banquete de ideias para</span> despertar o chef que há em você!
       </p>
 
-      <p class="subtitulo-lg">
-        Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
-      </p>
+      <p class="subtitulo-lg">Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!</p>
     </div>
 
     <img src="../assets/imagens/foto-banner.png"
