db0a752eb25a8203e324aef53909166bc868bde4
Gustavo Nunes Wawrick <gustavo_nw@outlook.com>
2025-02-26 21:11:10 -0300

    Finzalido o projeto para que consiga selecionar os ingredientes, pesquisar e retornar as receitas

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -4,15 +4,16 @@
       <img src="../assets/imagens/logo.svg" alt="Logo do Cookin' Up" class="logo">
 
       <p class="cabecalho-lg frase-cabecalho">
-        <span class="texto-verde">Um banquete de ideias para</span> despertar o chef que há em você!
+        <span class="texto-verde">Um banquete de ideias para</span>
+        despertar o chef que há em você!
       </p>
 
-      <p class="subtitulo-lg">Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!</p>
+      <p class="subtitulo-lg">
+        Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
+      </p>
     </div>
 
-    <img src="../assets/imagens/foto-banner.png"
-      alt="Foto de uma mulher cozinhando com uma bacia de vidro nas mãos. Ela está sorrindo e está apoiando o celular entre o rosto e o ombro."
-      class="foto-banner">
+    <img src="../assets/imagens/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidro nas mãos. Ela está sorrindo e está apoiando um celular entre seu rosto e ombro." class="foto-banner">
   </header>
 </template>
 
--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,97 +1,101 @@
 <script lang="ts">
+import MostrarReceitas from './MostrarReceitas.vue';
 import SelecionarIngredientes from './SelecionarIngredientes.vue';
+import SuaLista from './SuaLista.vue';
+import Tag from './Tag.vue';
+
+type Pagina = 'SelecionarIngredientes' | 'MostrarReceitas';
 
 export default {
-    data() {
-        return {
-            ingredientes: ['Alho', 'Manteiga', 'Orégano']
-        }
+  data() {
+    return {
+      ingredientes: [] as string[],
+      conteudo: 'SelecionarIngredientes' as Pagina
+    };
+  },
+  components: { SelecionarIngredientes, Tag, SuaLista, MostrarReceitas },
+  methods: {
+    adicionarIngrediente(ingrediente: string) {
+      this.ingredientes.push(ingrediente)
     },
-    components: { SelecionarIngredientes }
+    removerIngrediente(ingrediente: string) {
+      this.ingredientes = this.ingredientes.filter(iLista => ingrediente !== iLista);
+    },
+    navegar(pagina: Pagina) {
+      this.conteudo = pagina;
+    }
+  }
 }
 </script>
 
 <template>
-    <main class="conteudo-principal">
-        <section>
-            <span class="subtitulo-lg sua-lista-texto">Sua Lista:</span>
+  <main class="conteudo-principal">
+    <SuaLista :ingredientes="ingredientes" />
 
-            <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-                <li v-for="ingrediente in ingredientes" :key="ingrediente" class="ingrediente">{{ ingrediente }}</li>
-            </ul>
-
-            <p v-else class="paragrafo lista-vazia">
-                <img src="../assets/imagens/icones/lista-vazia.svg" alt="Ícone de pesquisa">
-                Sua lista está vazia, selecione ingredientes para iniciar.
-            </p>
-        </section>
-
-        <SelecionarIngredientes />
-
-    </main>
+    <KeepAlive include="SelecionarIngredientes">
+      <SelecionarIngredientes v-if="conteudo === 'SelecionarIngredientes'"
+        @adicionar-ingrediente="adicionarIngrediente"
+        @remover-ingrediente="removerIngrediente"
+        @buscar-receitas="navegar('MostrarReceitas')"
+      />
+  
+      <MostrarReceitas v-else-if="conteudo === 'MostrarReceitas'"
+        :ingredientes="ingredientes"
+        @editar-receitas="navegar('SelecionarIngredientes')"
+      />
+    </KeepAlive>
+  </main>
 </template>
 
 <style scoped>
 .conteudo-principal {
-    padding: 6.5rem 7.5rem;
-    border-radius: 3.75rem 3.75rem 0rem 0rem;
-    background: var(--creme, #FFFAF3);
-    color: var(--cinza, #444);
+  padding: 6.5rem 7.5rem;
+  border-radius: 3.75rem 3.75rem 0rem 0rem;
+  background: var(--creme, #FFFAF3);
+  color: var(--cinza, #444);
 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
 }
 
 .sua-lista-texto {
-    color: var(--coral, #F0633C);
-    display: block;
-    text-align: center;
-    margin-bottom: 1.5rem;
+  color: var(--coral, #F0633C);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
 }
 
 .ingredientes-sua-lista {
-    display: flex;
-    justify-content: center;
-    gap: 1rem 1.5rem;
-    flex-wrap: wrap;
-}
-
-.ingrediente {
-    display: inline-block;
-    border-radius: 0.5rem;
-    min-width: 4.25rem;
-    padding: 0.5rem;
-    text-align: center;
-    transition: 0.2s;
-    color: var(--creme, #FFFAF3);
-    background: var(--coral, #F0633C);
-    font-weight: 700;
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
 }
 
 .lista-vazia {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 0.25rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
 
-    color: var(--coral, #F0633C);
-    text-align: center;
+  color: var(--coral, #F0633C);
+  text-align: center;
 }
 
 @media only screen and (max-width: 1300px) {
-    .conteudo-principal {
-        padding: 5rem 3.75rem;
-        gap: 3.5rem;
-    }
+  .conteudo-principal {
+    padding: 5rem 3.75rem;
+    gap: 3.5rem;
+  }
 }
 
 @media only screen and (max-width: 767px) {
-    .conteudo-principal {
-        padding: 4rem 1.5rem;
-        gap: 4rem;
-    }
+  .conteudo-principal {
+    padding: 4rem 1.5rem;
+    gap: 4rem;
+  }
 }
 </style>
